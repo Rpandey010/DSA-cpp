e69d2325abc692457259ce5bf96cc28d49cca676
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-01-24 12:25:19 +0530

    patterns

---
+++ Pattern/16.cpp
@@ -0,0 +1,27 @@
+// ****
+// ***
+// **
+// *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int i = 1;
+    cin >> n;
+    while (i <= n)
+    {
+        int star = n - i + 1;
+        while (star)
+        {
+            cout << "*";
+            star = star - 1;
+        }
+
+        cout << endl;
+        i += 1;
+    }
+    return 0;
+}
