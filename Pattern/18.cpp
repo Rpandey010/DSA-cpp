126527732602674a794e00ae5ffb06dba169e19b
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-01-24 12:38:57 +0530

    done

--- Pattern/18.cpp
+++ Pattern/18.cpp
@@ -7,29 +7,60 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i=1;
+//     while(i<=n){
+//         //print 
+//         int space = n-i;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+
+//         }
+
+//         //print 1st traingle
+//         int j=1;
+//         while(j<=i){
+//             cout<<j;
+//             j=j+1;
+//         }
+
+//         //print 2nd triangle
+//         int start = i-1;
+//         while(start){
+//             cout<<start;
+//             start=start-1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+    
+
+// }
+
 int main(){
     int n;
     cin>>n;
 
     int i=1;
     while(i<=n){
-        //print 
-        int space = n-i;
+        //printing space
+        int space=n-i;
         while(space){
             cout<<" ";
             space=space-1;
-
         }
-
-        //print 1st traingle
+        //printing 1st triangle
         int j=1;
         while(j<=i){
             cout<<j;
             j=j+1;
         }
-
-        //print 2nd triangle
-        int start = i-1;
+        //printing 2nd traingle
+        int start=i-1;
         while(start){
             cout<<start;
             start=start-1;
@@ -37,6 +68,4 @@ int main(){
         cout<<endl;
         i=i+1;
     }
-    
-
 }
