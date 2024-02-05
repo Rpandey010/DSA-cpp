abbeab446d1dbd22e9912c3779652cbe9b656a6a
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-02-05 21:18:14 +0530

    Max Min

---
+++ Arrays/MaxMin.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int getMax(int num[], int n){
+
+    int max= INT16_MIN;
+
+    for(int i=0; i<n; i++){
+        if(num[i] > max){
+            max = num[i];
+        }
+    }
+    //returning max value
+    return max;
+}
+
+int getMin(int num[], int n){
+
+    // int min= INT16_MAX;
+    int mini= INT16_MAX;
+    
+    
+    for(int i=0; i<n; i++){
+
+        mini = min(mini, num[i]);
+        // if(num[i] < min){
+        //     min = num[i];
+        // }
+    }
+    //returning min value
+    // return min;
+    return mini;
+}
+
+
+int main(){
+
+    int size;
+    cin>>size;
+
+    int num[10];  //i am saying that the size/capacity of array is 10
+    //taking input in array
+    for(int i=0; i<size; i++){
+        if(i >= 100) {
+            cout << "Cannot enter more than 100 integers." << endl;
+            break;
+        }
+        cin>>num[i];
+    }
+
+    cout<<"Maximum value is "<< getMax(num, size) <<endl;
+    cout<<"Minimum value is "<< getMin(num, size) <<endl;
+
+    return 0;
+}
