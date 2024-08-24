70f16ff4e361ec089ceb0050b800babc0263684c
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-08-24 15:27:23 +0530

    pp

--- Random Practice/Basics/prime.cpp
+++ Random Practice/Basics/prime.cpp
@@ -29,9 +29,16 @@ int main(){
 }
 
 
+/*
 Why Check Only Up to sqrt(n)?
 
 Definition of Factors: If a number n is not prime, it can be factored into two integers: n = a * b.
 Symmetry of Factors: If a * b = n, then both a and b cannot both be greater than the square root of n. 
 This is because if they were, their product would exceed n. 
 Thus, one of these factors must be less than or equal to the square root of n.
+
+If n has a factor larger than its square root, the corresponding factor must be smaller than the square root.
+For example, if n = 36, the factors are (1, 36), (2, 18), (3, 12), (4, 9), and (6, 6). 
+Notice that once you reach the factor 6, you start seeing repeats in reverse order ((9, 4), etc.).
+Thus, if no factors are found up to sqrt(n), it means n is prime.
+*/
